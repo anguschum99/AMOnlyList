4cd013a04ff35938b3b3ed98caa1d6fb95319786
IRSTE <syracusebear@gmail.com>
2024-10-03 15:24:18 -0400

    - Partial MangaDetail implementation

--- app/src/main/java/com/example/mal/ui/screens/MangaDetail.kt
+++ app/src/main/java/com/example/mal/ui/screens/MangaDetail.kt
@@ -3,14 +3,27 @@ package com.example.mal.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun MangaDetail(
     uiState: MalUiState,
     modifier: Modifier = Modifier
 ){
-    Column(modifier = modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,modifier = modifier) {
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(uiState.currentManga?.data?.images?.jpg?.image_url)
+                .crossfade(true)
+                .build(),
+            contentDescription = null
+        )
+
+
         Text(text = uiState.currentManga?.data?.title ?: "null")
         Text(text = uiState.currentManga?.data?.synopsis.toString() ?: "null")
     }
